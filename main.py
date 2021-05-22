2b669b0c8b79524917882b8cad25474f647814f4
munjolla <franjotot@gmail.com>
2021-05-22 14:13:49 +0200

    topic_create

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import hashlib
 import uuid
 from flask import Flask, render_template, request, redirect, url_for, make_response
+
+from models.topic import Topic
 from models.user import User
 from models.settings import db
 
@@ -16,7 +18,74 @@ def index():
     session_token = request.cookies.get("session_token")
     user = db.query(User).filter_by(session_token=session_token).first()
 
-    return render_template("index.html", user=user)
+    # get topics
+    topics = db.query(Topic).all()
+
+    return render_template("index.html", user=user, topics=topics)
+
+
+@app.route("/create-topic", methods=["GET", "POST"])
+def topic_create():
+    if request.method == "GET":
+        return render_template("topic_create.html")
+
+    elif request.method == "POST":
+        title = request.form.get("title")
+        text = request.form.get("text")
+
+        # get current user (author)
+        session_token = request.cookies.get("session_token")
+        user = db.query(User).filter_by(session_token=session_token).first()
+
+        # only logged in users can create a topic
+        if not user:
+            return redirect(url_for('login'))
+
+        # create a Topic object
+        topic = Topic.create(title=title, text=text, author=user)
+
+        return redirect(url_for('index'))
+
+
+@app.route("/topic/<topic_id>", methods=["GET"])
+def topic_details(topic_id):
+    topic = db.query(Topic).get(int(topic_id))
+
+    # get current user (author)
+    session_token = request.cookies.get("session_token")
+    user = db.query(User).filter_by(session_token=session_token).first()
+
+    return render_template("topic_details.html", topic=topic, user=user)
+
+
+@app.route("/topic/<topic_id>/edit", methods=["GET", "POST"])
+def topic_edit(topic_id):
+    topic = db.query(Topic).get(int(topic_id))
+
+    if request.method == "GET":
+        return render_template("topic_edit.html", topic=topic)
+
+    elif request.method == "POST":
+        title = request.form.get("title")
+        text = request.form.get("text")
+
+        # get current user (author)
+        session_token = request.cookies.get("session_token")
+        user = db.query(User).filter_by(session_token=session_token).first()
+
+        # check if user is logged in and user is author
+        if not user:
+            return redirect(url_for('login'))
+        elif topic.author.id != user.id:
+            return "You are not the author!"
+        else:
+            # update the topic fields
+            topic.title = title
+            topic.text = text
+            db.add(topic)
+            db.commit()
+
+            return redirect(url_for('topic_details', topic_id=topic_id))
 
 
 @app.route("/login", methods=["GET", "POST"])
